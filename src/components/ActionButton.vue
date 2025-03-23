cab728da8a4b76ecfeddbe42e1a8284446422c17
zak <tsienyanel@gmail.com>
2025-03-23 19:53:30 +0200

    Added login button & profile image Added custom boxShadow to tailwind.config.js

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,12 @@
+
+
+<template>
+  <button  class="bg-brand-blue-1 rounded border-0 px-5 py-3 font-medium text-white hover:shadow-blue" >Sign in</button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+
+}
+</script>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,7 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
       <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-0">
-        <a :href="url"  class="flex h-full items-center text-xl">{{companyName}}</a>
+        <a :href="url"  class="flex h-full items-center text-xl font-semibold">{{companyName}}</a>
         <nav class="ml-12  h-full">
           <ul class="flex h-full list-none">
             <li class="h-full ml-9 first:ml-0" v-for="item in menuItems" :key="item">
@@ -10,14 +10,26 @@
             </li>
           </ul>
         </nav>
+        <div class="ml-auto flex h-full items-center">
+          <ProfileImage v-if="loggedIn" />
+          <ActionButton @click="loginUser()" v-else />
+
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from "@/components/ActionButton.vue";
+import ProfileImage from "@/components/ProfileImage.vue";
+
 export default {
   name: 'MainNav',
+  components: {
+    ActionButton,
+    ProfileImage,
+  },
   data() {
     return {
       companyName: "Google Careers",
@@ -29,8 +41,13 @@ export default {
           "Benefits",
           "Students",
       ],
-
+      loggedIn: false,
     }
+  },
+  methods: {
+    loginUser() {
+      this.loggedIn = true;
+    },
   }
 }
 
