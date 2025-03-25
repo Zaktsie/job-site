0f2e60bb75c5940f13bbc2420645e7e7bef7e2e2
zak <tsienyanel@gmail.com>
2025-03-25 21:11:32 +0200

    added login button & profile pic components. added unit tests for new components

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,7 +1,7 @@
 
 
 <template>
-  <button  class="bg-brand-blue-1 rounded border-0 px-5 py-3 font-medium text-white hover:shadow-blue" >Sign in</button>
+  <button  class="bg-brand-blue-1 rounded border-0 px-5 py-3 font-medium text-white hover:shadow-blue"  >Sign in</button>
 </template>
 
 <script>
