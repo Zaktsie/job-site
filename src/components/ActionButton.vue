3322548bc23283d6d1061e4a048648c976f3ac8f
zak <zakt@miway.co.za>
2025-04-12 15:31:35 +0200

    added props & used tailwind apply attri for header styles

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,12 +1,29 @@
 
 
 <template>
-  <button  class="bg-brand-blue-1 rounded border-0 px-5 py-3 font-medium text-white hover:shadow-blue"  >Sign in</button>
+  <button  class="primary"  >{{actionBtnText}}</button>
 </template>
 
 <script>
 export default {
   name: "ActionButton",
+  props: ["actionBtnText"]
 
 }
 </script>
+
+<style scoped>
+
+button {
+  @apply rounded border-0 px-5 py-3
+}
+.primary {
+  @apply border-0 bg-brand-blue-1 text-white  hover:shadow-blue
+}
+
+.secondary {
+
+}
+
+
+</style>
