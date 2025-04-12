1794b1c45e68bf0d3d8b861f6e7581d55b9bcb92
zak <zakt@miway.co.za>
2025-04-12 16:35:47 +0200

    added computed prop to dynamically change login button style

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,27 +1,36 @@
 
 
 <template>
-  <button  class="primary"  >{{actionBtnText}}</button>
+  <button  :class=" buttonClass"  >{{actionBtnText}}</button>
 </template>
 
 <script>
 export default {
   name: "ActionButton",
-  props: ["actionBtnText"]
-
+  props: ["actionBtnText"],
+  data() {
+    return {
+      primary: true,
+    };
+  },
+  computed:  {
+    buttonClass() {
+      return { primary: this.primary, secondary: !this.primary}
+    }
+  }
 }
 </script>
 
 <style scoped>
 
 button {
-  @apply rounded border-0 px-5 py-3
+  @apply rounded border-0 px-5 py-3 font-medium
 }
 .primary {
   @apply border-0 bg-brand-blue-1 text-white  hover:shadow-blue
 }
-
 .secondary {
+  @apply bg-transparent text-brand-blue-1 hover:bg-brand-blue-2 hover:text-white
 
 }
 
