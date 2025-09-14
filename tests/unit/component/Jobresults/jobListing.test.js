562ba17bcfd4526745192cf85a4b54678c6716d5
zak <tsienyanel@gmail.com>
2025-09-14 22:32:13 +0200

    update unit tests

--- tests/unit/component/Jobresults/jobListing.test.js
+++ tests/unit/component/Jobresults/jobListing.test.js
@@ -6,6 +6,8 @@ describe("JobListing.vue", () => {
   const createJobProps = (jobProps = {}) => ({
     title: "Vue Developer",
     organization: "Yokohama",
+    locations: ["Pretoria", "Remote"],
+    minimumQualifications: ["Coding", "Creating Api's"],
     ...jobProps,
   });
   const renderJobListing = (jJobProps) => {
@@ -26,6 +28,24 @@ describe("JobListing.vue", () => {
     const jobProps = createJobProps({ title: "Vue frontend dev" });
     renderJobListing(jobProps);
     expect(screen.getByText("Vue frontend dev")).toBeInTheDocument();
+  });
+
+  it('renders Job Qualifications"', () => {
+    const jobProps = createJobProps({
+      minimumQualifications: ["Coding", "Writing unit tests"],
+    });
+    renderJobListing(jobProps);
+    expect(screen.getByText("Coding")).toBeInTheDocument();
+    expect(screen.getByText("Writing unit tests")).toBeInTheDocument();
+  });
+
+  it("renders Job locations", () => {
+    const jobProps = createJobProps({
+      minimumQualifications: ["Cape Town", "Gauteng"],
+    });
+    renderJobListing(jobProps);
+    expect(screen.getByText("Cape Town")).toBeInTheDocument();
+    expect(screen.getByText("Gauteng")).toBeInTheDocument();
   });
 
   it("renders Job Organisation", () => {
